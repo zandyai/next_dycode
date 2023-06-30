fbc6c4dba8f24c34b6400788411917c28bcd337d
DyCodeBlock <iam.zandy88@gmail.com>
2023-06-30 12:05:35 +0800

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,26 @@
+import Navbar from '@/components/Navbar'
+import './globals.css'
+import Footer from '@/components/Footer'
+
+export const metadata = {
+  title: 'Dycode',
+  description: 'Showcase and discover remarkable developer projects',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <Navbar />
+        <main>
+          {children}
+        </main>
+        <Footer />
+      </body>
+    </html>
+  )
+}
